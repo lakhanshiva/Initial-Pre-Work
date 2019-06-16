6e4cee81cec782a95fdacdde964733e34090fd86
Lakhan Kamireddy <lakhanshiva@gmail.com>
2019-06-16 15:46:12 -0600

    Complete 81-85 in Javascript challenge

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+/*dot (.) notation is used to access values in javascript object*/
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
