6f51a807e92abf3023c72ece344e8f0c53bf3694
Lakhan Kamireddy <lakhanshiva@gmail.com>
2019-06-16 22:02:09 -0600

    Complete 93-95 in Javascript challenge

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+/*While loops in javascript*/
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i=0;
+while(i < 5){
+    myArray.push(i);
+    i++;
+}
---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+/*For loops in javascript*/
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var j=1; j<=5; j++) {
+  myArray.push(j);
+}
+
+window.alert(myArray);
