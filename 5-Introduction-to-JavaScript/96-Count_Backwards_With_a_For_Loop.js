485b862c4f27ced28e098f2d79e04e0adda2bf65
Lakhan Kamireddy <lakhanshiva@gmail.com>
2019-06-16 22:13:39 -0600

    Complete 96-100 in Javascript challenge

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,15 @@
+/*For loop i -= 2*/
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(i=9; i>0; i-=2){
+  myArray.push(i);
+}
