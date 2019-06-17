7e706832090cb2c2f6540a7746000596fc5c909a
Lakhan Kamireddy <lakhanshiva@gmail.com>
2019-06-16 22:34:51 -0600

    Complete 106-107 in Javascript challenge

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+//Ternary conditional operator e.g. (a==b) ? true : false;
+function checkEqual(a, b) {
+  return (a==b) ? true : false;
+}
+
+checkEqual(1, 2);
---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,6 @@
+//Using multiple ternary operators together e.g. (num == 0) ? "zero" : (num > 0) ? "positive" : "negative"
+function checkSign(num) {
+  return (num == 0) ? "zero" : (num > 0) ? "positive" : "negative";
+}
+
+checkSign(10);
